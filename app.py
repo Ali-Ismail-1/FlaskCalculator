072d2bc1eba0fbc603face77522b8a392fdbe083
Ali <Ali@Servers4Less.com>
2022-10-12 16:32:56 -0700

    added calculator page

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
